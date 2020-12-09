8833758e97efacaeab609bffabdbc8a993505668
Iago Martins <53104712+iagomartins@users.noreply.github.com>
2020-12-09 16:08:46 -0300

    Version 1.0 completed

--- src/components/Brandings.vue
+++ src/components/Brandings.vue
@@ -16,6 +16,155 @@
                     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
+                <h2 class="paragraph-title">Nossa marca</h2>
+                <p class="paragraph-words">
+                    Temos muito orgulho da nossa marca, portanto requisitamos que você siga corretamente as seguintes instruções para 
+                    garantir que ela seja sempre bem aplicada. Nossa marca é a combinação de um logotipo morderno e simples e um ícone.
+                </p>
+                <div class="logo-card">
+                    <img src="../assets/logo.svg" alt="">
+                </div>
+                <a href="#" class="btn btn-primary btn-download">Download da marca e icone</a>
+                <strong class="title-icon">Ícone</strong>
+                <p class="paragraph-words">
+                    Temos muito orgulho da nossa marca, portanto requisitamos que você siga corretamente as seguintes instruções para 
+                    garantir que ela seja sempre bem aplicada. Nossa marca é a combinação de um logotipo morderno e simples e um ícone.
+                </p>
+                <div class="big-bang-icons">
+                    <div class="icon-default">
+                        <img src="../assets/Brandings/icone-default.svg" alt="">
+                    </div>
+                    <div class="icon-default">
+                        <img src="../assets/Brandings/icone-black.svg" alt="">
+                    </div>
+                    <div class="icon-dark-background">
+                        <img src="../assets/Brandings/icone-dark-background.svg" alt="">
+                    </div>
+                    <div class="icon-white">
+                        <img src="../assets/Brandings/icone-white.svg" alt="">
+                    </div>
+                </div>
+                <a href="#" class="btn btn-primary btn-download">Download da marca e icone</a>
+                <h2 class="paragraph-title">Margem de segurança</h2>
+                <p class="paragraph-words">
+                    Temos muito orgulho da nossa marca, portanto requisitamos que você siga corretamente as seguintes instruções para 
+                    garantir que ela seja sempre bem aplicada. Nossa marca é a combinação de um logotipo morderno e simples e um ícone.
+                </p>
+                <div class="logo-card">
+                    <img src="../assets/logo.svg" alt="">
+                </div>
+                <a href="#" class="btn btn-primary btn-download">Download da marca e icone</a>
+                <h2 class="paragraph-title">Redução máxima</h2>
+                <p class="paragraph-words">
+                    Temos muito orgulho da nossa marca, portanto requisitamos que você siga corretamente as seguintes instruções para 
+                    garantir que ela seja sempre bem aplicada. Nossa marca é a combinação de um logotipo morderno e simples e um ícone.
+                </p>
+                <div class="logo-card">
+                    <img src="../assets/logo.svg" alt="">
+                </div>
+                <a href="#" class="btn btn-primary btn-download">Download da marca e icone</a>
+                <h2 class="paragraph-title">Usos incorretos</h2>
+                <p class="paragraph-words">
+                    Temos muito orgulho da nossa marca, portanto requisitamos que você siga corretamente as seguintes instruções para 
+                    garantir que ela seja sempre bem aplicada. Nossa marca é a combinação de um logotipo morderno e simples e um ícone.
+                </p>
+                <div class="logo-card">
+                    <img src="../assets/logo.svg" alt="">
+                </div>
+                <a href="#" class="btn btn-primary btn-download">Download da marca e icone</a>
+                <h2 class="paragraph-title">Nossas cores</h2>
+                <p class="paragraph-words">
+                    Temos muito orgulho da nossa marca, portanto requisitamos que você siga corretamente as seguintes instruções para 
+                    garantir que ela seja sempre bem aplicada. Nossa marca é a combinação de um logotipo morderno e simples e um ícone.
+                </p>
+                <strong class="title-info">Digital</strong>
+                <div class="colors-info">
+                    <div class="color blue-1">
+                        <div class="rectangle"></div>
+                        <div class="info">
+                            <strong>Azul 1</strong>
+                            <span>#2351ff</span>
+                            <span>R35 G81 B255</span>
+                        </div>
+                    </div>
+                    <div class="color blue-2">
+                        <div class="rectangle"></div>
+                        <div class="info">
+                            <strong>Azul 2</strong>
+                            <span>#1f6fff</span>
+                            <span>R35 G81 B255</span>
+                        </div>
+                    </div>
+                    <div class="color blue-3">
+                        <div class="rectangle"></div>
+                        <div class="info">
+                            <strong>Azul 3</strong>
+                            <span>#00bae9</span>
+                            <span>R35 G81 B255</span>
+                        </div>
+                    </div>
+                    <div class="color black">
+                        <div class="rectangle"></div>
+                        <div class="info">
+                            <strong>Preto</strong>
+                            <span>#0b2239</span>
+                            <span>R35 G81 B255</span>
+                        </div>
+                    </div>
+                </div>
+                <strong class="title-info">Impressos</strong>
+                <div class="colors-info">
+                    <div class="color blue-1-cmyk">
+                        <div class="rectangle"></div>
+                        <div class="info">
+                            <strong>Azul 1</strong>
+                            <span>C100 M87 Y0 K0</span>
+                            <span>Pantone 115-7U</span>
+                        </div>
+                    </div>
+                    <div class="color blue-2">
+                        <div class="rectangle"></div>
+                        <div class="info">
+                            <strong>Azul 2</strong>
+                            <span>C M Y K</span>
+                            <span>Pantone</span>
+                        </div>
+                    </div>
+                    <div class="color blue-3">
+                        <div class="rectangle"></div>
+                        <div class="info">
+                            <strong>Azul 3</strong>
+                            <span>C69 M3 Y0 K0</span>
+                            <span>Pantone</span>
+                        </div>
+                    </div>
+                    <div class="color black">
+                        <div class="rectangle"></div>
+                        <div class="info">
+                            <strong>Preto</strong>
+                            <span>C M Y K</span>
+                            <span>Pantone</span>
+                        </div>
+                    </div>
+                </div>
+                <h2 class="paragraph-title">Tipografia</h2>
+                <p class="paragraph-words">
+                    Temos muito orgulho da nossa marca, portanto requisitamos que você siga corretamente as seguintes instruções para 
+                    garantir que ela seja sempre bem aplicada. Nossa marca é a combinação de um logotipo morderno e simples e um ícone.
+                </p>
+                <strong class="title-info">Digital</strong>
+                <div class="mulish-panel">
+                    <span>Mulish</span>
+                </div>
+                <strong class="title-info">Impressos</strong>
+                <div class="print-panels">
+                    <div class="montserrat">
+                        <span>Montserrat Alternates</span>
+                    </div>
+                    <div class="mulish">
+                        <span>Mulish</span>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
@@ -33,11 +182,14 @@ export default {
         padding: 43px 0;
         margin-bottom: 100px;
     }
+    .guidelines .container {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
     .brandings-title {
         color: #0B2239;
         font-weight: 800;
-        font-size: 144px;
-        line-height: 124px;
         margin-bottom: 88px;
     }
     .blue-dot {
@@ -50,5 +202,171 @@ export default {
         font-size: 18px;
         line-height: 23px;
         max-width: 792px;
+    }
+    .paragraph-title {
+        font-weight: 600;
+        font-size: 36px;
+        line-height: 44px;
+        color: #2351FF;
+        margin-bottom: 32px;
+    }
+    .paragraph-words {
+        color: #3d3d3d;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 23px;
+        max-width: 792px;
+        margin-bottom: 32px;
+    }
+    .logo-card {
+        width: 100%;
+        padding: 120px 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        max-width: 792px;
+        background: #f3f3f3;
+        border-radius: 8px;
+        margin-bottom: 24px;
+    }
+    .btn-download {
+        background: #2351FF;
+        padding: 16px 32px;
+        font-weight: bold;
+        font-size: 15px;
+        line-height: 20px;
+        margin-bottom: 70px;
+    }
+    .title-icon {
+        color: #2351FF;
+        font-weight: 800;
+        font-size: 24px;
+        line-height: 30px;
+        margin-bottom: 32px;
+    }
+    .icon-default {
+        padding: 58px 40px;
+        background: #f3f3f3;
+        border-radius: 8px;
+        margin-bottom: 32px;
+    }
+    .icon-dark-background {
+        padding: 58px 40px;
+        background: #000;
+        border-radius: 8px;
+        margin-bottom: 32px;
+    }
+    .icon-white {
+        padding: 58px 40px;
+        background: #2351FF;
+        border-radius: 8px;
+        margin-bottom: 32px;
+    }
+    .color:not(:last-child) {
+        margin-bottom: 32px;
+    }
+    .title-info {
+        color: #2351FF;
+        font-weight: 800;
+        font-size: 24px;
+        line-height: 30px;
+        margin-bottom: 32px;
+    }
+    .colors-info {
+        padding: 32px;
+        background: #f3f3f3;
+        border-radius: 8px;
+        margin-bottom: 32px;
+    }
+    .rectangle {
+        display: block;
+        width: 144px;
+        height: 32px;
+        border-radius: 8px;
+        margin-bottom: 16px;
+    }
+    .blue-1 .rectangle {
+        background: #2351FF;
+    }
+    .blue-2 .rectangle {
+        background: #1f6fff;
+    }
+    .blue-3 .rectangle {
+        background: #00bae9;
+    }
+    .black .rectangle {
+        background: #0b2239;
+    }
+    .blue-1-cmyk .rectangle {
+        background: #1414D2;
+    }
+    .info {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        color: #666;
+        font-size: 16px;
+        line-height: 20px;
+    }
+    .info strong {
+        margin-bottom: 5px;
+    }
+    .mulish-panel {
+        background: #f3f3f3;
+        padding: 51px;
+        width: 100%;
+        max-width: 792px;
+        display: flex;
+        align-items: flex-start;
+        justify-content: flex-start;
+        height: 196px;
+        border-radius: 8px;
+        margin-bottom: 83px;
+    }
+    .print-panels {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: flex-start;
+        margin-bottom: 83px;
+        width: 100%;
+    }
+    .montserrat, .mulish {
+        padding: 51px;
+        width: 100%;
+        margin-bottom: 32px;
+        background: #f3f3f3;
+        height: 300px;
+        border-radius: 8px;
+    }
+    @media (min-width: 1200px) {
+        .big-bang-icons {
+            width: 100%;
+            max-width: 792px;
+            display: flex;
+            justify-content: space-between;
+        }
+        .color:not(:last-child) {
+            margin-bottom: 0;
+        }
+        .colors-info {
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+            max-width: 792px;
+        }
+        .print-panels {
+            flex-direction: row;
+            max-width: 792px;
+            justify-content: space-between;
+        }
+        .montserrat, .mulish {
+            width: 384px;
+            margin-bottom: 0;
+        }
+        .brandings-title {
+            font-size: 144px;
+            line-height: 124px;
+        }
     }
 </style>
